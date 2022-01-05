e9c0d4a67578d088eb2dab7ab98ea89a1088eddd
Pavle <jovanovicpavle00@gmail.com>
2022-01-05 13:28:27 +0100

    added class

--- js/rezervacije.js
+++ js/rezervacije.js
@@ -6,7 +6,7 @@ let number = document.querySelector("#number");
 let sumbit = document.querySelector("#submit");
 let form = document.querySelector(".form");
 let timeValue;
-console.log(time);
+// console.log(time);
 function checkedValue() {
 	for (let i = 0; i < time.length; i++) {
 		if (time[i].checked) {
@@ -16,12 +16,32 @@ function checkedValue() {
 	return timeValue;
 }
 checkedValue();
-
+console.log(checkedValue());
+class Rezervacija {
+	constructor(email, tel, film, number) {
+		email = this.email;
+		tel = this.tel;
+		film = this.film;
+		number = this.number;
+		// time = checkedValue();
+	}
+	displayInfo() {
+		console.log(email.value);
+	}
+}
 form.addEventListener("submit", function handleSubmit(e) {
 	e.preventDefault();
-	console.log(email.value);
-	console.log(tel.value);
-	console.log(film.value);
-	console.log(checkedValue());
-	console.log(number.value);
+	// console.log(email.value);
+	// console.log(tel.value);
+	// console.log(film.value);
+	// console.log(checkedValue());
+	// console.log(number.value);
+	let record = new Rezervacija(
+		email.value,
+		tel.value,
+		film.value,
+		number.value
+	);
+	record.displayInfo();
+	console.log("btn clicked");
 });
